964dfe0c14c478e0fcfbdb251314f479e5ea7f5c
Toanf2103 <nobitakute002@gmail.com>
2023-05-15 20:01:02 +0700

    them add xe

--- get.py
+++ get.py
@@ -26,4 +26,62 @@ def getAllXe():
     for row in rows:
         xe = {"maXe": row.maXe, "tenXe": row.tenXe, "hangXe": row.hangXe, "bienSoXe": row.bienSoXe, "loaiXe": row.loaiXe, "giaThue": row.trangThai, "trangThai": row.bienSoXe}
         data.append(xe)
+    conn.close()
     return data
+    
+
+def addXe(tenXe,hangXe,trangThai,bienSoXe,loaiXe,giaThue):
+    conn = connect()
+    cursor = conn.cursor
+    connection= conn.connection
+    try:
+        cursor.execute("{CALL pr_add_Xe(?, ?, ?, ?, ?, ?)}", (tenXe,hangXe,trangThai,bienSoXe,loaiXe,giaThue))
+        connection.commit()
+        conn.close()
+        
+        return {
+            'status': 'success'
+        }
+    except pyodbc.Error as ex:
+        conn.close()
+        return  {
+            'status': 'error',
+            'mess' : 'add fail'
+        }
+
+def checkLogin(username,password):
+    conn = connect()
+    cursor = conn.cursor
+    rs={}
+    sql=f"select * from TaiKhoan where taiKhoan='{username}'"
+    print(sql)
+    cursor.execute(sql)
+    record = cursor.fetchone()
+    if record is None:
+        rs = {
+            'status': 'error',
+            'mess' : 'username wrong'
+        }
+    elif record[2]!=password:
+        rs = {
+            'status': 'error',
+            'mess' : 'pass wrong',
+            'test':'test'
+        }
+    else:
+        rs = {
+            "maTaiKhoan":record[0],
+            "taiKhoan":record[1],
+            "matKhau":record[2],
+            "phanQuyen":record[3],
+            "trangThai":record[4],
+            "hoTen":record[5],
+            "ngaySinh":record[6],
+            "cccd":record[7],
+            "sdt":record[8],
+            "diaChi":record[9],
+            "gioiTinh":record[10],
+            "avatar":record[11]
+        }
+    conn.close()
+    return rs
--- index.py
+++ index.py
@@ -8,10 +8,17 @@ import get
 
 app = FastAPI()
 
+@app.post("/login")
+def login(username:str,password:str):
+    return get.checkLogin(username,password)
 
 @app.get("/getAll")
 def getall():
     return get.getAllXe()
+
+@app.post("/addXe")
+def addXe(tenXe:str,hangXe:str,trangThai:str,bienSoXe:str,loaiXe:str,giaThue:float):
+    return get.addXe(tenXe,hangXe,trangThai,bienSoXe,loaiXe,giaThue)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=5000)
