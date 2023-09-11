0c581f3a678ab4c8168aee3919c5b144003ecdec
Toanf2103 <nobitakute002@gmail.com>
2023-09-11 19:29:27 +0700

    edit connect

--- api/Connect.py
+++ api/Connect.py
@@ -3,10 +3,10 @@ class connect():
 
     def __init__(self):
             # Thông tin cơ sở dữ liệu
-        server = 'MSI'
-        database = 'QuanLyXeMay'
-        username = 'sa'
-        password = 'kutoan1346'
+        server = 'SQL5107.site4now.net'
+        database = 'db_a9ec38_quanlyxemay'
+        username = 'db_a9ec38_quanlyxemay_admin'
+        password = 'quanlyxemay1'
 
         # Tạo chuỗi kết nối
         connection_string = 'DRIVER={SQL Server};SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password
